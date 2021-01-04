031e63241ac30aadfa76db15f52f146595b907b0
Ollie Robinson <ollie@ollierobinson.co.uk>
2021-01-04 22:16:49 +0000

    Uploaded OpenTKStuffAgain

---
+++ OpenTKStuffAgain/OpenTKStuffAgain/ObjLoader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OpenTKStuffAgain
+{
+    public class ObjLoader
+    {
+        public float[] Vertices;
+        public float[] TexCoords;
+        public float[] Indices;
+        public ObjLoader(string path)
+        { 
+            using (var reader = new StreamReader(path, Encoding.UTF8))
+            {
+                List<float> vertices = new List<float>();
+                List<float> texCoords = new List<float>();
+                List<float> indices = new List<float>();
+
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var splitLine = line.Split(' ');
+                    if (splitLine[0] == "v") { foreach (var vertex in splitLine.Skip(1)) { vertices.Add(float.Parse(vertex)); } }
+                    if (splitLine[0] == "vt") { foreach (var coord in splitLine.Skip(1)) { texCoords.Add(float.Parse(coord)); } }
+                    if (splitLine[0] == "f") { 
+                        foreach (var num in splitLine.Skip(1))
+                        {
+                            var splitNum = num.Split('/');
+                            foreach(var num2 in splitNum) { indices.Add(float.Parse(num2)); }
+                        }
+                    }
+                }
+                Vertices = vertices.ToArray();
+                TexCoords = texCoords.ToArray();
+                Indices = indices.ToArray();
+            }
+        }
+    }
+}
---
+++ OpenTKStuffAgain/OpenTKStuffAgain/Texture.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using OpenTK.Graphics.OpenGL4;
+using PixelFormat = OpenTK.Graphics.OpenGL4.PixelFormat;
+
+
+namespace OpenTKStuffAgain
+{
+    public class Texture
+    {
+        public readonly int Handle;
+
+        // Create texture from path
+        public Texture(string path)
+        {
+            // Generate texture handle
+            Handle = GL.GenTexture();
+            // Bind texture handle
+            Use();
+
+            // Load the image
+            using (var image = new Bitmap(path))
+            {
+                // Get the pixels from the bitmap.
+                var data = image.LockBits(
+                    new Rectangle(0, 0, image.Width, image.Height),
+                    ImageLockMode.ReadOnly,
+                    System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+                // Generate texture
+                GL.TexImage2D(TextureTarget.Texture2D,
+                    0,
+                    PixelInternalFormat.Rgba,
+                    image.Width,
+                    image.Height,
+                    0,
+                    PixelFormat.Bgra,
+                    PixelType.UnsignedByte,
+                    data.Scan0);
+            }
+
+            // Set the min and mag texture filter
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+
+            // Set the texture wrapping mode
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+
+            // Generate mipmaps
+            GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+        }
+
+        // Activate textures
+        public void Use(TextureUnit unit = TextureUnit.Texture0)
+        {
+            GL.ActiveTexture(unit);
+            GL.BindTexture(TextureTarget.Texture2D, Handle);
+        }
+    }
+}
